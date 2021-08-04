a3235bc9f393552611e3d762e0faa34517bbefa5
John Beck <cygnus_33065@yahoo.com>
2021-08-04 19:04:28 -0400

    filter fragment views completed

--- app/src/main/java/com/example/catmemegenerator/CatMemeFilterFragment.kt
+++ app/src/main/java/com/example/catmemegenerator/CatMemeFilterFragment.kt
@@ -1,6 +1,41 @@
 package com.example.catmemegenerator
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
+import com.example.catmemegenerator.databinding.CatMemeFilterFragmentBinding
 
 class CatMemeFilterFragment: Fragment() {
+
+    private var binding: CatMemeFilterFragmentBinding? = null
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = CatMemeFilterFragmentBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val filters = resources.getStringArray(R.array.filters_array)
+
+        val spinner = binding?.spFilter
+        spinner?.let{
+            context?.let {
+                val adapter = ArrayAdapter(it, android.R.layout.simple_spinner_item, filters)
+                spinner.adapter = adapter
+            }
+        }
+
+
+    }
+
+
 }
